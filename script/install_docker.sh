e960dcd2ee4e21e83818d01c00c91ebaaec0d243
thanh15dt2 <luuvanthanh15dt2@gmail.com>
2021-01-24 10:48:03 +0700

    Add install_docker.sh file

---
+++ script/install_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash#
+
+# Add the GPG key for the official Docker repository to the system:
+echo 'Add the GPG key for the official Docker repository to the system!'
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Add the Docker repository to APT sources:
+echo 'Add the Docker repository to APT sources! '
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
+
+echo 'Installing...'
+sudo apt-get install -y docker-ce
+
+echo 'Adding group...'
+sudo usermod -aG docker ${USER}
+
+echo 'Installed successfully'
+
